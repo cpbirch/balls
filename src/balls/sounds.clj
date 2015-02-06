a3e5dfc0363aca5010159fd6ada2954ae8f9cfa2
Sandeep Aggarwal <may-be-it-works@maybe-it-dont>
2015-02-06 01:37:49 +0000

    Added ns to list available sounds

---
+++ src/balls/sounds.clj
@@ -0,0 +1,33 @@
+(ns balls.sounds
+  (:require [clojure.data.json :refer [read-str]]
+            [clojure.java.io :refer [as-file file]]
+            [clojure.string :as string]))
+
+(def sounds-dir "resources/public/sounds")
+
+(def web-path (partial str "/sounds/"))
+
+(defn- list-files [dir]
+  (-> dir file .listFiles))
+
+(defn- get-names [files]
+  (map #(.getName %) files))
+
+(defn- normalize [filename]
+  (-> filename
+      (string/split #"\.")
+      first
+      (string/replace #"_", " ")
+      (string/replace #"\W", " ")
+      (string/replace #"\s+", " ")))
+
+(defn- normalized-map [filenames]
+  (->> filenames
+       (map (juxt normalize web-path))
+       (into {})))
+
+(defn sounds []
+  (-> sounds-dir
+      list-files
+      get-names
+      normalized-map))
