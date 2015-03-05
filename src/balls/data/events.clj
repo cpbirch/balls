7cf7a97a95189f950d188e0098ed02268d6030de
Sandeep Aggarwal <may-be-it-works@maybe-it-dont>
2015-03-05 20:42:07 +0000

    Weather conditions now have sunny-cloudy effect

--- src/balls/data/events.clj
+++ src/balls/data/events.clj
@@ -3,6 +3,7 @@
 (def default-snow-effect-threshold 3)
 (def default-blizzard-effect-threshold 4)
 (def default-cloud-effect-threshold 1)
+(def default-night-effect-threshold 2)
 (def default-red-alert-threshold 5)
 (def default-glitch-effect-threshold 6)
 
@@ -15,6 +16,7 @@
 (def snow-effect-over-threshold? (partial count-over-threshold? default-snow-effect-threshold))
 (def blizzard-effect-over-threshold? (partial count-over-threshold? default-blizzard-effect-threshold))
 (def cloud-effect-over-threshold? (partial count-over-threshold? default-cloud-effect-threshold))
+(def night-effect-over-threshold? (partial count-over-threshold? default-night-effect-threshold))
 
 (defn- add-red-alert [red-alert-threshold {:keys [sick] :as grouped-pipelines}]
   (assoc grouped-pipelines :red-alert (red-alert-over-threshold? red-alert-threshold sick)))
@@ -31,8 +33,8 @@
 (defn- add-cloud-effect [{:keys [sick sick-building] :as grouped-pipelines}]
   (assoc grouped-pipelines :clouds (cloud-effect-over-threshold? nil (into sick sick-building))))
 
-(defn- add-sun-effect [{:keys [sick sick-building] :as grouped-pipelines}]
-  (assoc grouped-pipelines :night (cloud-effect-over-threshold? nil (into sick sick-building))))
+(defn- add-night-effect [{:keys [sick sick-building] :as grouped-pipelines}]
+  (assoc grouped-pipelines :night (night-effect-over-threshold? nil (into sick sick-building))))
 
 (defn add-ui-events [{:keys [red-alert-threshold glitch-effect-threshold]} grouped-pipelines]
   (->> grouped-pipelines
@@ -41,4 +43,5 @@
       add-snow-effect
       add-blizzard-effect
       add-cloud-effect
-      add-sun-effect))
+      add-night-effect
+      ))
