03800938f60378ee620358ff01e2defbdef3a056
Brydon Cheyney <brydon@remission.org.uk>
2016-03-11 21:20:51 +0000

    Brydon - added core ns to run application in jetty container

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Used to create template for basic clojure server project"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.5"]
                  [hiccup "1.0.4"]
@@ -10,14 +10,18 @@
                  [ring/ring-json "0.3.1"]
                  [org.clojure/data.json "0.2.5"]
                  [base64-clj "0.1.1"]
-                 [clj-http "1.1.2"]]
+                 [clj-http "1.1.2"]
+                 [environ "1.0.2"]
+                 [ring/ring-jetty-adapter "1.4.0"]]
 
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]
-                   :plugins [[lein-midje "3.1.3"]]}}
+  :profiles {:dev     {:dependencies [[midje "1.6.3"]]
+                       :plugins      [[lein-midje "3.1.3"]]}}
 
   :plugins [[lein-ring "0.9.4"]]
 
   :aliases {"test" ["midje"]}
 
-  :ring {:handler balls.app/app})
+  :ring {:handler balls.app/app}
+
+  :main balls.core)
 
